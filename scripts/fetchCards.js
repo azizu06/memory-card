5fc91eb869507b6ac155429500f2790e41c5c2fa
azizu06 <ab725492@ucf.edu>
2026-02-25 18:28:51 -0500

    gather all CR cards in json format

---
+++ scripts/fetchCards.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+import "dotenv/config";
+
+const key = process.env.CR_UCF_KEY;
+if (!key) throw new Error("Missing CR Key in env");
+
+const res = await fetch("https://api.clashroyale.com/v1/cards", {
+  headers: { Authorization: `Bearer ${key}` },
+});
+
+const { items } = await res.json();
+
+const cards = items.map((item) => ({
+  id: item.id,
+  name: item.name,
+  img: item.iconUrls?.medium,
+}));
+
+fs.writeFileSync("public/cards.json", JSON.stringify(cards, null, 2));
