cfd07055aee5b2d515f058deb00d00c86e21f3a4
azizu06 <ab725492@ucf.edu>
2026-02-26 23:29:37 -0500

    add sound handler

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -1,9 +1,14 @@
 import { Volume2, VolumeOff } from "lucide-react";
 
-export const Sound = ({ soundOn }) => {
+export const Sound = ({ sound, setSound }) => {
   return (
-    <div className="fixed top-4 right-4 z-[1000]">
-      {soundOn ? <Volume2 /> : <VolumeOff />}
+    <div className="fixed top-4 right-4 border z-[1000]">
+      <button
+        className="p-3 rounded-full"
+        onClick={() => setSound((prev) => !prev)}
+      >
+        {sound ? <Volume2 size={32} /> : <VolumeOff size={32} />}
+      </button>
     </div>
   );
 };
