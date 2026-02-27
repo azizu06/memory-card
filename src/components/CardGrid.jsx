7923c8430e9f6fe6f7bfd583a6c6ae3e677df756
azizu06 <ab725492@ucf.edu>
2026-02-27 02:22:25 -0500

    apply 3d flips for cards

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,7 +1,7 @@
 import { Card } from "./card";
 export const CardGrid = ({ cards, flipped, playRound }) => {
   return (
-    <div className="grid gap-4 border-orange-500 border-[6px] grid-cols-4 grid-rows-3 justify-center max-w-3xl">
+    <div className="grid gap-6 border-orange-500 border-[6px] grid-cols-4 grid-rows-3 justify-items-center justify-center ">
       {cards.map((card) => (
         <Card
           key={card.id}
