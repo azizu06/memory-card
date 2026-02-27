b5cccc72fc6f97c11a3962aec2f44ee74fbcfab3
azizu06 <ab725492@ucf.edu>
2026-02-27 01:00:58 -0500

    render cards in grid

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,5 +1,15 @@
-import { useState } from "react";
 import { Card } from "./card";
-export const CardGrid = () => {
-  return <div></div>;
+export const CardGrid = ({ cards, flipped, playRound }) => {
+  return (
+    <div className="grid gap-4 border-orange-500 border-[6px] grid-cols-4 grid-rows-3 justify-center max-w-3xl">
+      {cards.map((card) => (
+        <Card
+          key={card.id}
+          data={card}
+          flipped={flipped}
+          playRound={playRound}
+        />
+      ))}
+    </div>
+  );
 };
